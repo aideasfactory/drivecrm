838b2ff81bc83694d5f09861a888516fa2b3f2f1
sam@aideasfactory.io <sam@aideasfactory.io>
2026-03-23 11:47:41 +0000

    image changes

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
-import { Car } from 'lucide-vue-next';
 </script>
 
 <template>
     <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
+        class="flex aspect-square size-8 items-center justify-center rounded-md"
     >
-        <Car class="h-6 w-6 text-white" />
+        <AppLogoIcon
+            class="size-9 fill-current text-[var(--foreground)] dark:text-white"
+        />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="text-xl font-bold">DRIVE</span>
